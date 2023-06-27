ff89ae165c96c9ec6c711af7477307db49389c37
Ghufran <ghufranshahid101@gmail.com>
2023-06-26 20:02:45 -0400

    Changed from manually creating card components to calling function component of card

--- memorycard/src/App.tsx
+++ memorycard/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import './stylesheets/App.scss';
+import Card from './components/Card';
 
 function App() {
 
@@ -10,16 +11,8 @@ function App() {
     const cardArr: React.ReactElement[] = [];
 
     for(let i = 0; i < 12; i+= 1){
-      const card = <div className="max-w-sm min-h-[24rem] rounded overflow-hidden shadow-lg hover:outline hover:outline-2 hover:outline-offset-8 hover:outline-red-400">
-        <div className="w-full min-h-[40%] bg-slate-200 text-center mb-2">Img PlaceHolder</div>
-        <div className="px-4 py-2 mb-4">
-          <div className="font-bold text-xl mb-2">Card Title</div>
-          <div className="text-l">Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text</div>
-          <button className="text-center mt-8 border-2 border-red-500 rounded-full px-3 py-1 text-sm hover:bg-red-500 hover:text-white focus:ring-2 focus:ring-red-400">
-            Select
-          </button>
-        </div>
-      </div>
+      
+      const card = <Card name='bruh'></Card>
 
       cardArr.push(card)
       
